2d6202a1b58b87848cc60ad9708d03e20d399aba
Abhishekfc <abhisheksir6280@gmail.com>
2025-09-09 01:22:41 +0530

    worked on landing page

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,12 +1,18 @@
 import { Button } from '@/components/ui/button'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Carousel, CarouselContent, CarouselItem, } from "@/components/ui/carousel"
+import companies from "../data/companies.json"
+import faqs from "../data/faq.json"
+import Autoplay from "embla-carousel-autoplay"
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from "@/components/ui/card"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger, } from "@/components/ui/accordion"
 
 const LandingPage = () => {
   return <main className='flex flex-col gap-10 sm:gap-20 py-10 sm:py-20'>
     <section className='text-center'>
-      <h1 className='flex flex-col justify-center items-center gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter  py-4'> Find your dream Job <span 
-      className='flex items-center gap-2 sm:gap-6'>and get {" "}
+      <h1 className='flex flex-col justify-center items-center gradient-title text-4xl font-black sm:text-6xl lg:text-8xl tracking-tighter  py-4'> Find your dream Job <span
+        className='flex items-center gap-2 sm:gap-6'>and get {" "}
         <img
           src="/logo.png"
           alt='hirrd logo'
@@ -22,23 +28,71 @@ const LandingPage = () => {
     <div className='flex justify-center  gap-6'>
       {/* buttons  */}
       <Link to="/jobs">
-      <Button variant="blue" size="xl">Find Jobs</Button>
+        <Button variant="blue" size="xl">Find Jobs</Button>
       </Link>
       <Link to="/post-job">
-      <Button variant="destructive" size="xl">Post Job</Button>
+        <Button variant="destructive" size="xl">Post Job</Button>
       </Link>
-      
+
     </div>
-      {/* carousel */}
+    {/* carousel */}
+    <Carousel plugins={[
+      Autoplay({
+        delay: 2000,
+      }),
+    ]} className="w-full py-10">
+      <CarouselContent className="flex gap-5 sm:gap-10 items-center">
+        {companies.map(({ name, id, path }) => {
+          return <CarouselItem key={id} className="basis-1/3 lg:basis-1/6">
+            <img src={path}
+              alt={name}
+              className='h-9 sm:h-14 object-contain' />
+          </CarouselItem>
+        })}
+      </CarouselContent>
+
+    </Carousel>
 
     {/* banner */}
+    <img src="/banner.jpeg" alt="" className='w-full' />
 
-    <section>
+    <section className='grid gird-cols-1 md:grid-cols-2 gap-4'>
       {/* cards */}
+      <Card>
+        <CardHeader>
+          <CardTitle>For Job Seekers</CardTitle>
+        </CardHeader>
+        <CardContent>
+          Seaarch and apply for jobs, track applications, and more.
+        </CardContent>
+
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>For Employers</CardTitle>
+        </CardHeader>
+        <CardContent>
+          Post jobs, manage applications, and find the best candidates.
+        </CardContent>
+
+      </Card>
     </section>
 
     {/* accodian */}
+    <Accordion type="single" collapsible>
+      {
+        faqs.map(( faq, index) => {
+          return( <AccordionItem key={index} value={`item-${index + 1}`}>
+            <AccordionTrigger>{faq.question}</AccordionTrigger>
+            <AccordionContent>{faq. answer}</AccordionContent>
+          </AccordionItem>)
+        })}
+    </Accordion>
   </main>
 }
 
+{/* <AccordionTrigger>Is it accessible?</AccordionTrigger>
+<AccordionContent>
+  Yes. It adheres to the WAI-ARIA design pattern.
+</AccordionContent> */}
 export default LandingPage
