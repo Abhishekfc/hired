494134f3cdfb37c26bac2154e05784aca4b9c9d0
Abhishekfc <abhisheksir6280@gmail.com>
2025-09-25 23:48:21 +0530

    worked on saved job

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -2,28 +2,67 @@ import supabaseClient from "@/utils/supabase";
 import { commonjs } from "globals";
 import { LocationEditIcon } from "lucide-react";
 
-export async function getJobs(token, {location, company_id, searchQuery}) {
-    const supabase = await supabaseClient(token);  
+export async function getJobs(token, { location, company_id, searchQuery }) {
+    const supabase = await supabaseClient(token);
 
     let query = supabase.from("jobs").select("*, company:companies(name, logo_url) , saved: saved_jobs(id   )");
 
-    if(location){
+    if (location) {
         query = query.eq("location", location)
     }
-    if(company_id){
+    if (company_id) {
         query = query.eq("company_id", company_id)
     }
-    if(searchQuery){
+    if (searchQuery) {
         query = query.ilike("title", `%${searchQuery}%`)
     }
 
 
-    const {data, error} = await query;
+    const { data, error } = await query;
 
-    if(error) {
+    if (error) {
         console.error("Error fetching jobs:", error);
         return null;
     }
 
     return data;
+}
+
+export async function saveJob(token, { alreadySaved }, saveData) {
+    const supabase = await supabaseClient(token);
+
+    let query = supabase.from("jobs").select("*, company:companies(name, logo_url) , saved: saved_jobs(id   )");
+
+    if (alreadySaved) {
+        const { data, error: deleteError } = await supabase
+            .from("saved_jobs")
+            .delete()
+            .eq("job_id", saveData.job_id);
+
+        if (deleteError) {
+            console.error("Error deleting saved job:", deleteError);
+            return null;
+        }
+
+        return data;
+    }
+    else {
+        const { data, error: insertError } = await supabase
+            .from("saved_jobs")
+            .insert([saveData])
+            .select()
+
+        if (insertError) {
+            console.error("Error fetching jobs:", insertError);
+            return null;
+        }
+
+        return data;
+    }
+
+
+
+
+
+    
 }
--- src/components/job-card.jsx
+++ src/components/job-card.jsx
@@ -1,18 +1,42 @@
 import { useUser } from '@clerk/clerk-react'
-import React from 'react'
+import React, { use, useEffect, useState } from 'react'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from './ui/card';
 import { Heart, MapPinIcon, Trash2Icon } from 'lucide-react';
 import { Button } from './ui/button';
 import { Link } from 'react-router-dom';
+import useFetch from '@/hooks/use-fetch';
+import { saveJob } from '@/api/apiJobs';
 
 const JobCard = ({
     job,
     isMyJob = false,
     savedInit = false,
-    onjobSaved = () => { },
+    onJobSaved = () => {},
 }) => {
 
+    const [saved, setSaved] = useState(savedInit);
+
+    const { fn: fnSavedJob,
+        data: savedJob,
+        loading: loadingSavedJob,
+    } = useFetch(saveJob, {
+        alreadySaved: saved,
+    });
+
     const { user } = useUser();
+
+    const handleSaveJob = async () => {
+        await fnSavedJob({
+            user_id: user.id,
+            job_id: job.id,
+        });
+        onJobSaved();
+    }
+
+    useEffect(() => {
+        if (savedJob !== undefined) setSaved(savedJob?.length > 0);
+    }, [savedJob])
+
 
     return <Card>
         <CardHeader>
@@ -41,12 +65,28 @@ const JobCard = ({
 
         <CardFooter className="flex gap-2">
             <Link to={`/job/*{job.id}`} className='flex-1'>
-                    <Button variant="secondary" className ='w-full'>
-                        More Details
-                    </Button>
+                <Button variant="secondary" className='w-full'>
+                    More Details
+                </Button>
             </Link>
 
-            <Heart size={20}  stroke='red' fill='red'/>
+            {!isMyJob && (
+                <Button
+                    variant="outline"
+                    className="w-15"
+                    onClick={handleSaveJob}
+                    disabled={loadingSavedJob}>
+
+                    {saved ? (
+                        <Heart size={20} stroke='red' fill='red' />
+                    ) : (
+                        <Heart size={20} />
+                    )}
+
+
+                </Button>
+            )}
+
 
         </CardFooter>
     </Card>
